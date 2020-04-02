5052fcb9d0b2c7155a7ad3c12da10dc051037684
spaceman007 <xtimespace@163.com>
2020-04-02 11:34:10 +0800

    inline-source-map for development

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  devtool: 'inline-source-map'
 })
